65f919ba5ff3bc3bd0ed93287e04a5c7862ae595
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2023-10-09 21:38:12 +0200

    jo lesz

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # eaFC24
-Ez a weboldal röviden bemutatja az új EA FC 24 játékot
+Ez a weboldal röviden bemutatja az új EA FC 24-ben elérhető Ultimate Team újdonságait.
--- payment.js
+++ payment.js
@@ -4,10 +4,22 @@ function validateForm(){
         document.getElementById("content").style.display = 'block';
     }
     let card_holder = document.forms["payment"]["name"].value;
-    let number = Number(document.forms["payment"]["number"].value);
-    let cv = Number(document.forms["payment"]["cv"].value);
+    let number = document.forms["payment"]["number"].value;
+    let cv = document.forms["payment"]["cv"].value;
     if (card_holder == "" || number == "" || cv == ""){
         alert("Fill all fields!");
+        return false;
+    }
+    else if (isNaN(number) || isNaN(cv)){
+        alert('Enter correct data');
+        return false;
+    }
+    else if (number.length < 16 || number.length > 19){
+        alert('Card number must be between 16 and 19 characters long')
+        return false;
+    }
+    else if (cv.length != 3){
+        alert('CV number must be 3 characters long')
         return false;
     }
     document.getElementById("payment").style.display = 'none';
